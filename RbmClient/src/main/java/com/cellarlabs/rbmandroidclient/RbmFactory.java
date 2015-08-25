70a2fd540f806985c174abd8de923bd752b97425
flogvit <Vegard.Hanssen@menneske.no>
2015-08-25 09:44:25 +0200

    Moved to engine.io 0.6.0 and added setURI

--- RbmClient/src/main/java/com/cellarlabs/rbmandroidclient/RbmFactory.java
+++ RbmClient/src/main/java/com/cellarlabs/rbmandroidclient/RbmFactory.java
@@ -4,13 +4,18 @@ package com.cellarlabs.rbmandroidclient;
 public class RbmFactory {
 
     private static RbmAndroidClient sRbmAndroidClient;
+    private static String uri = "wss://api.puzzleall.com:8680";
 
     public static RbmAndroidClient getRbmClient(){
         if(sRbmAndroidClient == null){
             sRbmAndroidClient = new RbmAndroidClient();
-            sRbmAndroidClient.withServer("wss://api.puzzleall.com:8680");
+            sRbmAndroidClient.withServer(uri);
         }
         return sRbmAndroidClient;
     }
 
+    public static void setURI(String uri) {
+        RbmFactory.uri = uri;
+        sRbmAndroidClient.withServer(uri);
+    }
 }
